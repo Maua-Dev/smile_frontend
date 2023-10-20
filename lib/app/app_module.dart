0be90c3238b0c97f0c236c5b3bf3b2a55b10d26d
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-10-19 22:57:23 -0300

    setup pubspec

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
@@ -6,6 +7,9 @@ class AppModule extends Module {
 
   @override
   void routes(r) {
-    //  r.child('/', child: (context) => HomePage()),
+    r.child('/',
+        child: (context) => Container(
+              color: Colors.red,
+            ));
   }
 }
